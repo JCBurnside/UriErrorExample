4db3df4a0057da259860571787e8a5f04c6039d0
James Burnside <jcburnside97@gmail.com>
2018-04-11 14:15:59 -0400

    Add project files.

---
+++ UriErrorExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UriErrorExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
